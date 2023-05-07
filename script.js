02f80ab0636c770d17bcd47c7f254de103970980
Gh0sther <gorka6493@gmail.com>
2023-05-07 02:49:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.getElementById("btn__iniciar-sesion").addEventListener("click",iniciarS
 document.getElementById("btn__registrarse").addEventListener("click",register);
 
 
+
+
 var contenedor_login_register = document.querySelector(".contenedor__login-register");
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
@@ -9,19 +11,54 @@ var caja_trasera_login= document.querySelector(".caja__trasera-login");
 var caja_trasera_resgister= document.querySelector(".caja__trasera-register");
 
 
+function anchoPagina()
+    if(window.innerWidth >850){
+        caja_trasera_login.style.display="block";
+        caja_trasera_resgister.style.display="block";
+    }else {
+        caja_trasera_resgister.style.display="block";
+        caja_trasera_resgister.styel.opacity ="1";
+        caja_trasera_login.style.display ="none";
+        formulario_login.style.display="block";
+        formulario_register.style.display="none";
+        contenedor_login_register.style.left="0px";
+    }
+    anchoPagina();
+
 function iniciarSesion(){
+
+    if(window.innerWidth >850){
+
     formulario_register.style.display= "none";
     contenedor_login_register.style.left= "10px";
     formulario_login.style.display= "block";
     caja_trasera_resgister.style.opacity = "1";
     caja_trasera_login.style.opacity ="0";
+     } else{
+    formulario_register.style.display= "none";
+    contenedor_login_register.style.left= "0px";
+    formulario_login.style.display= "block";
+    caja_trasera_resgister.style.display = "block";
+    caja_trasera_login.style.display ="none";
+    
+    }
 }
 
 
 function register(){
+
+    if(window.innerWidth >850){
     formulario_register.style.display= "block";
     contenedor_login_register.style.left= "410px";
     formulario_login.style.display= "none";
     caja_trasera_resgister.style.opacity = "0";
     caja_trasera_login.style.opacity ="1";
+    }else{
+    formulario_register.style.display= "block";
+    contenedor_login_register.style.left= "0px";
+    formulario_login.style.display= "none";
+    caja_trasera_resgister.style.display = "none";
+    caja_trasera_login.style.display ="block";
+    caja_trasera_login.style.opacity= "1"
+    }
 }
