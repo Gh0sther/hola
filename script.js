67b179aee1fa68496304d3b6771c4e71f9dfb938
Gh0sther <gorka6493@gmail.com>
2023-05-07 02:10:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("hola mundo");
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var caja_trasera_login= document.querySelector(".caja_trasera-login");
+var caja_trasera_resgister= document.querySelector(".caja_trasera-register");
+
+
+function register(){
+    formulario_register.style.display= "block";
+    contenedor_login_register.style.left= "410px";
+    formulario_login.style.display= "none";
+    caja_trasera_resgister.style.opacity = "0";
+    caja_trasera_login.style.opacity ="1";
+}
