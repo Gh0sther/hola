867fd8a7c7fe866e5ed8f2d247b399ffb4a361dc
Gh0sther <gorka6493@gmail.com>
2023-05-07 02:14:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById("btn__registarse").addEventListener("click",register);
+
 var contenedor_login_register = document.querySelector(".contenedor__login-register");
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
