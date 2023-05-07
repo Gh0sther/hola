6f7340d2cb851ef42b45dfc6478119e66373bf1c
Gh0sther <gorka6493@gmail.com>
2023-05-07 03:04:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,54 +11,28 @@ var caja_trasera_login= document.querySelector(".caja__trasera-login");
 var caja_trasera_resgister= document.querySelector(".caja__trasera-register");
 
 
-function anchoPagina()
-    if(window.innerWidth >850){
-        caja_trasera_login.style.display="block";
-        caja_trasera_resgister.style.display="block";
-    }else {
-        caja_trasera_resgister.style.display="block";
-        caja_trasera_resgister.style.opacity ="1";
-        caja_trasera_login.style.display ="none";
-        formulario_login.style.display="block";
-        formulario_register.style.display="none";
-        contenedor_login_register.style.left="0px";
-    }
-anchoPagina();
+
 
 function iniciarSesion(){
 
-    if(window.innerWidth >850){
+   
 
     formulario_register.style.display= "none";
     contenedor_login_register.style.left= "10px";
     formulario_login.style.display= "block";
     caja_trasera_register.style.opacity = "1";
     caja_trasera_login.style.opacity ="0";
-     } else{
-    formulario_register.style.display= "none";
-    contenedor_login_register.style.left= "0px";
-    formulario_login.style.display= "block";
-    caja_trasera_register.style.display = "block";
-    caja_trasera_login.style.display ="none";
-    
-    }
+     
 }
 
 
 function register(){
 
-    if(window.innerWidth >850){
+ 
     formulario_register.style.display= "block";
     contenedor_login_register.style.left= "410px";
     formulario_login.style.display= "none";
     caja_trasera_resgister.style.opacity = "0";
     caja_trasera_login.style.opacity ="1";
-    }else{
-    formulario_register.style.display= "block";
-    contenedor_login_register.style.left= "0px";
-    formulario_login.style.display= "none";
-    caja_trasera_resgister.style.display = "none";
-    caja_trasera_login.style.display ="block";
-    caja_trasera_login.style.opacity= "1"
-    }
+   
 }
