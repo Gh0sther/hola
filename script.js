6158e0df70a0041965177ac0c007c36a2223e3a0
Gh0sther <gorka6493@gmail.com>
2023-05-07 02:23:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById("btn__registrarse").addEventListener("click",register);
 var contenedor_login_register = document.querySelector(".contenedor__login-register");
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
-var caja_trasera_login= document.querySelector(".caja_trasera-login");
-var caja_trasera_resgister= document.querySelector(".caja_trasera-register");
+var caja_trasera_login= document.querySelector(".caja__trasera-login");
+var caja_trasera_resgister= document.querySelector(".caja__trasera-register");
 
 
 function register(){
